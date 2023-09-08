55d344834c2db07aaeb169a202fb452bdbfd3882
RustCohle27 <instantcrushxx97@gmail.com>
2023-09-08 08:48:56 -0600

    ejercicios corregidos

---
+++ 1-Python/Tema3/Practica/Ejercicio5b.py
@@ -0,0 +1,7 @@
+def wants_plain_hotdog(ketchup, mustard, onion):
+    """Return whether the customer wants a plain hot dog with no toppings.
+    """
+    pass
+    return not (ketchup or mustard or onion)
+
+# Check your an
--- pandas/panda-csv.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-wind_reviews = pd.read_csv("winemag-data-130k-v2.csv")
-print(wind_reviews)
